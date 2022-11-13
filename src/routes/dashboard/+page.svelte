d7967bbb4272df6c57e4f931d6deef8c50e16d24
Abelito1 <abelitolim12@gmail.com>
2022-11-13 22:22:23 +0800

    edit mobile

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -85,7 +85,7 @@
 	</main>
 	<div class="px-4 mt-6 sm:px-6 lg:px-8">
 		<h2 class="text-gray-500 text-xs font-medium uppercase tracking-wide">Pinned</h2>
-		<ul role="list" class="grid grid-cols-1 gap-12 sm:gap-6 sm:grid-cols-2 xl:grid-cols-4 mt-3">
+		<ul class="grid grid-cols-1 gap-12 sm:gap-6 sm:grid-cols-2 xl:grid-cols-4 mt-3">
 			<li class="relative col-span-1 flex shadow-sm rounded-md">
 				<div
 					class="flex-shrink-0 flex items-center justify-center w-16 bg-pink-600 text-white text-sm font-medium rounded-l-md"
